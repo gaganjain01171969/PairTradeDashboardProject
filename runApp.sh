bda25bcf16fed507554aa9d5722c7ae1449c3f8f
Gagan Jain <Gagan.Jain@marsh.com>
2020-12-21 11:08:30 -0500

    removed path to directory

--- runApp.sh
+++ runApp.sh
@@ -1,5 +1,1 @@
-pip3 install pandas
-pip3 install numpy
-pip3 install matplotlib
-pip3 install wikipedia
-gunicorn --bind=0.0.0.0 --timeout 1800 --chdir "pairtradeaptml/web-application" main:app
+gunicorn --bind=0.0.0.0 --timeout 1800 main:app
