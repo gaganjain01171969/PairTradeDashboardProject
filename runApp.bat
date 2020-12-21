6f310eaa241454bc2c2b0cfdd422c0a9c2018df4
Gagan Jain <Gagan.Jain@marsh.com>
2020-12-21 10:36:45 -0500

    removed path to directory

--- runApp.bat
+++ runApp.bat
@@ -1,2 +1,1 @@
-pip3 install -r "pairtradeaptml/web-application"/requirements.txt
-gunicorn --bind=0.0.0.0 --timeout 1800 --chdir "pairtradeaptml/web-application" main:app
+gunicorn --bind=0.0.0.0 --timeout 1800 main:app
